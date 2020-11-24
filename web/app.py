fcc237b9c4d85e39f6627c7ade97738267f308b5
jurgas <Sebastianjurga7@gmail.com>
2020-11-24 02:15:36 +0100

    add cors, bug fix sender create - add email

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response, session
 from flask_session import Session
+from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 from bcrypt import hashpw, gensalt, checkpw
 from os import getenv
@@ -18,6 +19,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = getenv("SECRET_KEY")
 ses = Session(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 def create_response(msg, status):
@@ -31,13 +34,14 @@ def user_exists(username):
     return False
 
 
-def save_user(username, password, firstname, lastname, address):
+def save_user(username, password, firstname, lastname, email, address):
     salt = gensalt(4)
     password = password.encode()
     hashed_pass = hashpw(password, salt)
     db.hset(f"user:{username}", "password", hashed_pass)
     db.hset(f"user:{username}", "firstname", firstname)
     db.hset(f"user:{username}", "lastname", lastname)
+    db.hset(f"user:{username}", "email", email)
     db.hset(f"user:{username}", "address", address)
     return True
 
@@ -58,20 +62,23 @@ def save_label(label_id, username, receiver, size, po_box_id):
     return True
 
 
-@app.route('/sender/available', methods=["GET"])
+@app.route('/sender/available', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def sender_available():
     username = request.json.get('username')
     if user_exists(username):
-        return create_response("Username already exists", 400)
+        return create_response("Username already exists", 409)
     return create_response("Available", 200)
 
 
 @app.route('/sender/create', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def sender_create():
     firstname = request.json.get('firstname')
     lastname = request.json.get('lastname')
     username = request.json.get('username')
     password = request.json.get('password')
+    email = request.json.get('email')
     address = request.json.get('address')
 
     PL = 'ĄĆĘŁŃÓŚŹŻ'
@@ -84,19 +91,24 @@ def sender_create():
         return create_response("Invalid username", 400)
     if not re.compile('.{8,}').match(password.strip()):
         return create_response("Invalid password", 400)
+    if not re.compile(
+            '(?:[A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])').match(
+        email):
+        return create_response("Invalid email", 400)
     if address is None:
         return create_response("Empty address", 400)
 
     if user_exists(username):
         return create_response("Username already exists", 400)
 
-    if not save_user(username, password, firstname, lastname, address):
+    if not save_user(username, password, firstname, lastname, email, address):
         return create_response("An error occurred", 500)
 
     return create_response("Account created", 201)
 
 
 @app.route('/sender/label', methods=["GET"])
+@cross_origin(supports_credentials=True)
 def sender_get_labels():
     if not session.get("username"):
         return create_response("Unauthorized", 401)
@@ -118,11 +130,11 @@ def sender_get_labels():
                 "POBoxId": po_box_id
             }
             data.append(json_string)
-
-    return make_response("{" + f"{data}" + "}", 200)
+    return make_response({"data": data}, 200)
 
 
 @app.route('/sender/label', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def sender_add_label():
     if not session.get("username"):
         return create_response("Unauthorized", 401)
@@ -150,6 +162,7 @@ def sender_add_label():
 
 
 @app.route('/sender/label/<label_id>', methods=["DELETE"])
+@cross_origin(supports_credentials=True)
 def sender_delete_label(label_id):
     if not session.get("username"):
         return create_response("Unauthorized", 401)
@@ -169,6 +182,7 @@ def sender_delete_label(label_id):
 
 
 @app.route('/auth/login', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def auth_login():
     username = request.json.get('username')
     password = request.json.get('password')
@@ -183,9 +197,18 @@ def auth_login():
 
 
 @app.route('/auth/logout', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def auth_logout():
     session.clear()
     return create_response("Logout success", 200)
+
+
+@app.route('/auth/logged', methods=["GET"])
+@cross_origin(supports_credentials=True)
+def auth_logged_in():
+    if not session.get("username"):
+        return create_response("Unauthorized", 401)
+    return create_response("Is logged in", 200)
 
 
 if __name__ == '__main__':
